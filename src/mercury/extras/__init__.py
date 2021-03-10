2b015f65fbfc3a1341c8ae63f2fd4b8e79dc762f
Sylver <sylver@materya.io>
2021-03-10 07:33:33 +0100

    fix: add missing extras module init file

---
+++ src/mercury/extras/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019 - 2021 Richard Kemp
+# $Id$
+# -*- coding: utf-8; py-indent-offset:4 -*-
+
+"""Mercury Extras module."""
+
+from __future__ import annotations
+
+__copyright__ = "Copyright 2019 - 2021 Richard Kemp"
+__revision__ = "$Id$"
